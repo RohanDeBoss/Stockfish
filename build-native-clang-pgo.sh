c7a4ae0cbabab06aa5ba516887c95785369eaaf8
Rohan <rohanzakrichardson@gmail.com>
2026-05-15 09:51:09 +1000

    Update build commands to modern standards

--- build-native-clang-pgo.sh
+++ build-native-clang-pgo.sh
@@ -4,12 +4,10 @@ set -e
 cd /s/src
 
 mingw32-make clean
-mingw32-make -j profile-build ARCH=native COMP=clang
-strip stockfish.exe
-mv -f stockfish.exe stockfish-windows-native-clang-pgo.exe
+mingw32-make -j build COMP=clang
 
-./stockfish-windows-native-clang-pgo.exe bench
+./stockfish.exe bench
 
 echo
 echo "Arena engine path:"
-echo "C:\\Users\\ric0016\\OneDrive - Templestowe College\\Desktop\\Documents\\GitHub\\Stockfish\\src\\stockfish-windows-native-clang-pgo.exe"
+echo "C:\\Users\\ric0016\\OneDrive - Templestowe College\\Desktop\\Documents\\GitHub\\Stockfish\\src\\stockfish.exe"
